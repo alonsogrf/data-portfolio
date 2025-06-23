8418ee470741e69c9615372e6394d30ab3609767
Alonso Guerrero <83784369+alonsogrf@users.noreply.github.com>
2025-06-22 23:48:01 -0600

    Update ost.sql

--- ost.sql
+++ ost.sql
@@ -1,4 +1,4 @@
--- This logic is based on real-world challenges I addressed in a previous role. 
+-- This logic replicates real-world challenges I addressed in a previous role. 
 -- ⚠️ All have been anonymized to ensure confidentiality.
 
 ----- NEW PRODUCT OST -----
@@ -17,14 +17,14 @@ with list as (
     select 
         stage.macro_id,
         stage.stage_id as sales_stage_id,
-        -- Formatting converts the date to local time using a custom function
+        -- Formatting converts timestamps to local time via a custom function.
         formatting(stage.created_at) as sales_creation_date
     from {{ source('source','stages') }} as stages
     where stage.type = 'postSales'
 ),
 
 -- New Product Leads: Filters only customers linked to the product of interest, extracting relevant IDs.
--- • One customer may appear multiple times if they’ve acquired the product more than once.
+-- • A single customer may appear multiple times if they’ve acquired the product more than once.
 -- • The unique identifier for each product cycle is `sales_stage_id`.
 newProduct_leads as ( 
     select
@@ -95,7 +95,7 @@ sales_milestones as (
         -- Assumption:
         --    a. A 'no' is always explicitly documented when the customer is not interested.
         --    b. A 'yes' may be left undocumented if the team moves forward with the process.
-        -- Therefore, any blank entry is treated as an implicit 'yes' (customer is interested).
+        -- Therefore, blank entries are treated as implicit 'yes' (customer is interested).
         coalesce(docs.data->>'customerInterested' != 'no', true) as new_has_interest
     from newProduct_leads
         inner join {{ source('source','docs') }} as docs on docs.stage_id = newProduct_leads.sales_stage_id
@@ -111,7 +111,7 @@ delivery_milestones as (
 --   • As mentioned earlier, the `macro_id` in the `newProduct_leads` table may appear multiple times:
 --      > Each `macro_id` represents a customer and repeats for every product they have, identified by its unique `sales_stage_id`.
 --   • That's why this DISTINCT ON is used: to ensure only one row per `delivery_stage_id` is shown.
---      > Instead of one row per sales_stage_id × delivery_stage_id pair
+--      > Instead of one row per `sales_stage_id` × `delivery_stage_id` pair.
 --   • The ORDER BY clause ensures that the most recent `sales_stage_id` (prior to `delivery_stage_id`) is the one linked to
 --     that unique `delivery_stage_id`.
     select distinct on (delivery_stage.stage_id)
@@ -148,7 +148,7 @@ delivery_milestones as (
 
 -- Previous Attributes: Captures the original specs before the new product was acquired. 
 previous_attributes as (
-    -- • DISTINCT ON is needed because Project History logs an entry each time the billing status changes. I want to return a single record per `project_id`.
+    -- • DISTINCT ON is needed because `project_history` logs an entry each time the billing status changes. I want to return a single record per `project_id`.
     select distinct on (project_history.project_id)
         project_history.project_id,
         project_history.contract_id as previous_contract_id,
@@ -163,14 +163,14 @@ previous_attributes as (
 design_attributes as (
     select 
         newProduct_leads.sales_stage_id,
-        -- Numeric processing ensures unit consistency with `original_attributes` from `previous_attributes` CTE.
+        -- This step ensures unit consistency with `original_attributes` from `previous_attributes` CTE.
         round((attributes.data->>'someAttributes')::numeric*10000,2) as new_design_attributes
     from newProduct_leads
         inner join {{ source('source','docs') }} as attributes on attributes.stage_id = newProduct_leads.sales_stage_id
             and design.doc_type in ('designAttributes')
 )
 
--- Main Table: Built based on the declared principles:
+-- Final output: Built based on the declared principles:
 --    > Use DISTINCT ON to report a single sales cycle per product: one sales stage matched with one delivery stage.
 --    > Each product is shown as a separate record, resulting in multiple entries per customer.
 --    > Milestones are primarily retrieved from the delivery stage. If unavailable, fallback to the sales stage.
